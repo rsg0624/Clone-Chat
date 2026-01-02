3a0d4d22c229cd018f07d3dd0033d2dc8f3608fa
Rahul Ganti <rsg0624@gmail.com>
2026-01-02 10:15:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from engine.intent import detect_intent
 from engine.emotion import detect_emotion
